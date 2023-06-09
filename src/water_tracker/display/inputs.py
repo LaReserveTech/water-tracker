8455c50e033377b0b7fd4203c71290cbb5052829
grigaut <gaetanrigaut.s@gmail.com>
2023-06-09 18:48:51 +0200

    Ajout des inputs de date et période

--- src/water_tracker/display/inputs.py
+++ src/water_tracker/display/inputs.py
@@ -1,5 +1,6 @@
 """Inputs objects."""
 
+import datetime as dt
 from abc import ABC, abstractmethod
 from functools import cached_property
 from typing import TYPE_CHECKING, Any, Generic, TypeVar
@@ -7,13 +8,17 @@ from typing import TYPE_CHECKING, Any, Generic, TypeVar
 import pandas as pd
 
 from water_tracker.display.defaults import (
+    DefaultDate,
     DefaultDepartement,
     DefaultInput,
+    DefaultMaxDate,
+    DefaultMinDate,
     DefaultStation,
 )
 
 if TYPE_CHECKING:
     from streamlit.delta_generator import DeltaGenerator
+    from streamlit.elements.time_widgets import DateWidgetReturn
 
 
 DefaultInputT = TypeVar("DefaultInputT", bound="DefaultInput")
@@ -186,3 +191,144 @@ class StationInput(BaseInput[DefaultStation]):
             index=self.options.index(self._default.value),
             format_func=format_func,
         )
+
+
+class DateInput(BaseInput[DefaultDate]):
+    """Date input.
+
+    Parameters
+    ----------
+    label : str
+        Input label.
+    default_input : DefaultDate
+        Default value for the input.
+    min_value : dt.date
+        Minimum date.
+    max_value : dt.date
+        Maximum date.
+    key : str
+        Key for the date input.
+    """
+
+    def __init__(
+        self,
+        label: str,
+        default_input: DefaultDate,
+        min_value: dt.date,
+        max_value: dt.date,
+        key: str,
+    ) -> None:
+        super().__init__(label, default_input)
+        self._min = min_value
+        self._max = max_value
+        self._key = key
+
+    def build(self, container: "DeltaGenerator") -> "DateWidgetReturn":
+        """Build the input in a given container.
+
+        Parameters
+        ----------
+        container : DeltaGenerator
+            Container to build the input object to.
+
+        Returns
+        -------
+        DateWidgetReturn
+            Input value.
+        """
+        return container.date_input(
+            label=self.label,
+            value=self._default.value,
+            min_value=self._min,
+            max_value=self._max,
+            key=self._key,
+        )
+
+
+class PeriodInput:
+    """Input Period.
+
+    Parameters
+    ----------
+    label_min : str
+        Label for the minimum date selector.
+    label_max : str
+        Label for the maximum date selector.
+    min_date : dt.date
+        Minimum possible date.
+    max_date : dt.date
+        Maximum Possible date.
+    """
+
+    def __init__(
+        self,
+        label_min: str,
+        label_max: str,
+        min_date: dt.date,
+        max_date: dt.date,
+        min_default: DefaultMinDate,
+        max_default: DefaultMaxDate,
+    ) -> None:
+        self.label_min = label_min
+        self.label_max = label_max
+        self._min = min_date
+        self._max = max_date
+        self._min_default = min_default
+        self._max_default = max_default
+
+    def compute_min_start(
+        self,
+        min_chosen_date: "DateWidgetReturn",
+    ) -> dt.date:
+        """Compute minimum date for max date input.
+
+        Parameters
+        ----------
+        min_chosen_date : DateWidgetReturn
+            Date chosen in min date input.
+
+        Returns
+        -------
+        dt.date
+            Minimum chosen date if instance of date.
+        """
+        if isinstance(min_chosen_date, dt.date):
+            return min_chosen_date
+        return self._min
+
+    def build(
+        self,
+        container: "DeltaGenerator",
+    ) -> tuple["DateWidgetReturn", "DateWidgetReturn"]:
+        """Build the input in a given container.
+
+        Parameters
+        ----------
+        container : DeltaGenerator
+            Container to build the input object to.
+
+        Returns
+        -------
+        tuple["DateWidgetReturn", "DateWidgetReturn"]
+            Minimum date, maximum date, to use as input values.
+        """
+        min_col, max_col = container.columns(2)
+
+        min_input = DateInput(
+            label=self.label_min,
+            default_input=self._min_default,
+            min_value=self._min,
+            max_value=self._max,
+            key="date_min_input",
+        )
+        min_chosen = min_input.build(min_col)
+
+        max_input = DateInput(
+            label=self.label_max,
+            default_input=self._max_default,
+            min_value=self.compute_min_start(min_chosen),
+            max_value=self._max,
+            key="date_max_input",
+        )
+        max_chosen = max_input.build(max_col)
+        return min_chosen, max_chosen
