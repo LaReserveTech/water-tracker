01a30e0e35a9097dac0ce693225aa75c3e94292e
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-01-24 14:57:53 +0100

    Update streamlit.py
    Début d'explication

--- streamlit.py
+++ streamlit.py
@@ -1,2 +1,16 @@
 import streamlit as st
 st.title('Water Tracker')
+
+st.sidebar.radio('Indicateurs de niveaux d'eau', [":rainbow["Sècheresse"]", "Autre indicateur" , "Water Tracker:movie_camera:"])
+pages = ['page1', 'page2', 'page3']
+
+connection = connection to db / addresse de files dans S3 (dispo sous url)
+
+if page == pages[0]:
+  #things to put on the page
+if page == pages[1]:
+  #things to put on the second page
+if page == pages[2]:
+  #things to put on the third page
+
+  
