c9d07613309be905e7ebb0302ba1d3c4f1e9c826
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-02-03 12:58:46 +0100

    Update streamlit.py
    Remove connection to db added first titles

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -8,48 +8,22 @@ import plotly.express as px
 
 ## Début du display
 st.title('Water Tracker')
+st.header('Statistiques et visualisations des données sécheresse en France métropolitaine en 2023')
+st.write('Water Tracker est un outil permettant de suivre l’évolution de la sécheresse et ses impacts, en France métropolitaine sur l’année 2023.')
+
+st.title('ÉVOLUTION DE LA SÉCHERESSE EN 2023')
+st.write('Un épisode de sécheresse peut survenir si nos ressources en eau sont en tension ou ont été en tension pendant une trop longue période. Nous disposons de 3 ressources en eau principales :
+
+- La pluie
+- Les nappes phréatiques
+- Les eaux de surface (fleuves, rivières, lacs)
+
+Pour plus d’informations sur la sécheresse [rdv ici](%s)'%(https://www.ecologie.gouv.fr/secheresse).)
 
 st.sidebar.title('Navigation')
 st.sidebar.radio('Indicateurs de niveaux d'eau', [":rainbow["Sècheresse"]", "Autre indicateur" , "Water Tracker:movie_camera:"])
 pages = ["Sècheresse", "Autre indicateur" , "Water Tracker"]
 
-
-# connection = connection to db / addresse de files dans S3 (dispo sous url)
-[gcp_service_account]
-type = "service_account"
-project_id = "xxx"
-private_key_id = "xxx"
-private_key = "xxx"
-client_email = "xxx"
-client_id = "xxx"
-auth_uri = "https://accounts.google.com/o/oauth2/auth"
-token_uri = "https://oauth2.googleapis.com/token"
-auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs"
-client_x509_cert_url = "xxx"
-
-# streamlit_app.py
-
-import streamlit as st
-from google.oauth2 import service_account
-from google.cloud import bigquery
-
-# Create API client.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-client = bigquery.Client(credentials=credentials)
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def run_query(query):
-    query_job = client.query(query)
-    rows_raw = query_job.result()
-    # Convert to list of dicts. Required for st.cache_data to hash the return value.
-    rows = [dict(row) for row in rows_raw]
-    return rows
-
-rows = run_query("SELECT word FROM `bigquery-public-data.samples.shakespeare` LIMIT 10")
 
 # Print results.
 st.write("Some wise words from Shakespeare:")
