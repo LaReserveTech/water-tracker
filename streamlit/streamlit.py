7bf536cbda65818f19684a9b69aae9de7f7ffbaf
Sarah Le Net <sarahlunette@users.noreply.github.com>
2024-02-04 01:54:49 +0100

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -27,7 +27,7 @@ Pour plus d’informations sur la sécheresse [rdv
 ici](https://www.ecologie.gouv.fr/secheresse).''')
 
 if page == pages[1]:
-
+    st.title('Les impacts de la sècheresse en 2023')
 
 
 if page == pages[2]:
